429ef997e37c08bbea72ac94dc5c5e26b7f59947
= <=>
2022-02-08 12:37:51 +0800

    adding welcome api

--- main.go
+++ main.go
@@ -1,6 +1,28 @@
 package main
 
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/hananloser/moflip/model"
+)
+
 // Main Function
 func main() {
 
+	app := fiber.New()
+	// Enable Every Each Request
+	app.Use(logger.New(logger.Config{
+		Format: "[${ip}]:${port} ${status} - ${method} ${path}\n",
+	}))
+
+  // Welcome API
+	app.Get("/", func(c *fiber.Ctx) error {
+    return c.JSON(model.WebResponse{
+      Code: 200,
+      Status: "success",
+      Data: "Moflip API",
+    })
+	})
+
+	app.Listen(":3000")
 }
